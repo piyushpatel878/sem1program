8d2119fae1c304b8599dffe6f3db984c3b93cce2
piyushpatel878 <piyush.visnagar@gmail.com>
2026-08-28 15:46:25 +0530

    Create p79.c

---
+++ p79.c
@@ -0,0 +1,12 @@
+//Print squares of numbers from 1 to 10 in reverse
+#include<stdio.h>
+int main()
+{
+    int n,i,mul;
+    for(i=1;i<=10;i++){
+    mul=i*i;
+    printf("%d",mul);
+    printf("\n");
+    }
+    return 0;
+    }
