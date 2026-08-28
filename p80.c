737196f993f65ffeb3723adec0a60728641d7fad
piyushpatel878 <piyush.visnagar@gmail.com>
2026-08-28 15:46:57 +0530

    Create p80.c

---
+++ p80.c
@@ -0,0 +1,12 @@
+//Print cube of numbers from 1 to 10 in reverse
+#include<stdio.h>
+int main()
+{
+    int n,i,mul;
+    for(i=1;i<=10;i++){
+    mul=i*i*i;
+    printf("%d",mul);
+    printf("\n");
+    }
+    return 0;
+    }
