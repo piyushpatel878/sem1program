56bde5d355a4e222b12958090bfb0e3413681c21
piyushpatel878 <piyush.visnagar@gmail.com>
2026-08-28 15:15:04 +0530

    Create p75.c

---
+++ p75.c
@@ -0,0 +1,12 @@
+ //print n number in reverse
+#include<stdio.h>
+int main()
+{
+    int n, i;
+    printf("Enter your number:");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--){
+    printf("%d",i);
+    printf("\n");   }
+     return 0;
+}
