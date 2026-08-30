ce644895c7d2754ce9a1d87643ff6b6b2a9b67a2
piyushpatel878 <piyush.visnagar@gmail.com>
2026-08-30 09:06:55 +0530

    Update p96.c

--- p96.c
+++ p96.c
@@ -1,8 +1,8 @@
-//Print all factors of a number.
+//Print all factors  of a number.(sum and count)
 #include<stdio.h>
 int main()
 {
-     int n,i,count=0,r;
+     int n,i,count=0,r,sum=0;
     printf("Enter your number:");
     scanf("%d",&n);
     for(i=1;i<=n;i++)
@@ -11,11 +11,14 @@ int main()
     r=n%i;
     if(r==0){
     count++;
+    sum=sum+i;
     
     
     printf("factors =%d\n",i);}}
     printf("\n");
-    printf("number of factors is %d",count);
+    printf("number of factors is %d\n",count);
+    printf("sum of factors is %d",sum);
+    
     return 0;
 }    
     
