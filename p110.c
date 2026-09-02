025497509f25533d070691a4b5b5ff728139a836
piyushpatel878 <piyush.visnagar@gmail.com>
2026-09-02 12:48:17 +0530

    Create p110.c

---
+++ p110.c
@@ -0,0 +1,13 @@
+//right triangle pattern
+#include<stdio.h>
+int main()
+{
+    int n,j,i;
+    printf("Enter your number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+    for(j=1;j<=i;j++)
+    printf("*");
+    printf("\n");}  
+    return 0;
+    }
